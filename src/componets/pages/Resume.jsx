1c882cf0e6251a1ab76983cd8c54140d6aa4cf31
Benjamin Brechter <benbrechter3@gmail.com>
2024-06-13 13:24:14 -0500

    resume fix

--- src/componets/pages/Resume.jsx
+++ src/componets/pages/Resume.jsx
@@ -1,10 +1,12 @@
-import resumePdf from "../images/resume.jpg"
+import resumePdf from "../images/resume1.png"
+import resumePdf2 from "../images/resume2.png"
 
 export default function Resume() {
     return (
-        <div>
+        <div className="resume-box">
             <h1 class="header" style={{marginTop: "150px", marginBottom: "30px"}}>Resume</h1>
-          <img src={resumePdf} class = "resume"/>
+          <img src={resumePdf} className = "resume"/>
+          <img src={resumePdf2} className = "resume"/>
         
         </div>
     );

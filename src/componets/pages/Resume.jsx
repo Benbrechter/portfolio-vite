3202bd9ad5d37ed4f0fe0b0a3c59ac2d9586d97e
Benjamin Brechter <benbrechter3@gmail.com>
2026-07-14 15:55:48 -0500

    idk

--- src/componets/pages/Resume.jsx
+++ src/componets/pages/Resume.jsx
@@ -1,12 +1,12 @@
-import resumePdf from "../images/resume1.png"
-import resumePdf2 from "../images/resume2.png"
+import resumePdf from "../images/resume.pdf"
+// import resumePdf2 from "../images/resume2.png"
 
 export default function Resume() {
     return (
         <div className="resume-box">
             <h1 class="header" style={{marginTop: "150px", marginBottom: "30px"}}>Resume</h1>
           <img src={resumePdf} className = "resume"/>
-          <img src={resumePdf2} className = "resume"/>
+          {/* <img src={resumePdf2} className = "resume"/> */}
         
         </div>
     );
